3cbacb3f4e7c6a5b4ed3499d07c040a066b63bc8
Volodymyr Polishchuk <rot010011@gmail.com>
2025-03-20 18:54:13 +0200

    3rd homework is done. Forgot to save changes in previous commit

--- homework_03.py
+++ homework_03.py
@@ -1,4 +1,15 @@
-alice_in_wonderland = '"Would you tell me, please, which way I ought to go from here?"\n"That depends a good deal on where you want to get to," said the Cat.\n"I don't much care where ——" said Alice.\n"Then it doesn't matter which way you go," said the Cat.\n"—— so long as I get somewhere," Alice added as an explanation.\n"Oh, you're sure to do that," said the Cat, "if you only walk long enough."'
+import math
+
+alice_in_wonderland = """\
+"Would you tell me, please, which way I ought to go from here?"
+"That depends a good deal on where you want to get to," said the Cat.
+"I don't much care where ——" said Alice.
+"Then it doesn't matter which way you go," said the Cat.
+"—— so long as I get somewhere," Alice added as an explanation.
+"Oh, you're sure to do that," said the Cat, 
+"if you only walk long enough."'
+"""
+print(alice_in_wonderland)
 # task 01 == Розділіть змінну alice_in_wonderland так, щоб вона займала декілька фізичних лінії
 # task 02 == Знайдіть та відобразіть всі символи одинарної лапки (') у тексті
 # task 03 == Виведіть змінну alice_in_wonderland на друк
@@ -16,7 +27,9 @@ alice_in_wonderland = '"Would you tell me, please, which way I ought to go from 
 моря становить 37 800 км2. Яку площу займають Чорне та Азов-
 ське моря разом?
 """
-
+black_sea_area_size = 436402
+azov_sea_area_size = 37800
+print(black_sea_area_size + azov_sea_area_size)
 
 # task 05
 """
@@ -25,7 +38,11 @@ alice_in_wonderland = '"Would you tell me, please, which way I ought to go from 
 250 449 товарів. На другому та третьому – 222 950 товарів.
 Знайдіть кількість товарів, що розміщені на кожному складі.
 """
-
+goods_total = 375291
+first_depot_amount = goods_total - 222950  # віднімаємо від загальної кількості суму 2 і 3 складів
+third_depot_amount = goods_total - 250449  # віднімаємо від загальної кількості суму 1 і 2 складів
+second_depot_amount = goods_total - first_depot_amount - third_depot_amount
+print(f"first depot = {first_depot_amount}, second depot = {second_depot_amount}, third depot = {third_depot_amount}")
 
 # task 06
 """
@@ -34,7 +51,9 @@ alice_in_wonderland = '"Would you tell me, please, which way I ought to go from 
 вати необхідно буде півтора року по 1179 грн/місяць. Обчисліть
 вартість комп’ютера.
 """
-
+months_amount = 18
+monthly_payment = 1179
+print(months_amount * monthly_payment)
 
 # task 07
 """
@@ -43,7 +62,14 @@ a) 8019 : 8     d) 7248 : 6
 b) 9907 : 9     e) 7128 : 5
 c) 2789 : 5     f) 19224 : 9
 """
-
+print(
+    8019 % 8,
+    9907 % 9,
+    2789 % 5,
+    7248 % 6,
+    7128 % 5,
+    19224 % 9
+)
 
 # task 08
 """
@@ -57,6 +83,12 @@ c) 2789 : 5     f) 19224 : 9
 Торт            1           350 грн
 Вода            3           21 грн
 """
+pizza_large = 4 * 274
+pizza_medium = 2 * 218
+juice = 4 * 35
+cake = 1 * 350
+water = 3 * 21
+print(pizza_large + pizza_medium + juice + cake + water)
 
 
 # task 09
@@ -66,7 +98,10 @@ c) 2789 : 5     f) 19224 : 9
 розміщено щонайбільше 8 фото. Скільки сторінок знадобиться
 Ігорю, щоб вклеїти всі фото?
 """
-
+photo_amount = 232
+page_limit = 8
+page_amount = math.ceil(photo_amount / page_limit)  # math.ceil() у разі, якщо результатом ділення кількості фото на ліміт фотографій буде не ціле число
+print(page_amount)
 
 # task 10
 """
@@ -79,3 +114,9 @@ c) 2789 : 5     f) 19224 : 9
 равку під час цієї подорожі, кожного разу заправляючи пов-
 ний бак?
 """
+distance = 1600
+petrol_consumption_per_100km = 9
+tank_capacity = 48
+total_petrol_needed = (distance / 100) * petrol_consumption_per_100km
+refill_count = math.ceil(total_petrol_needed / tank_capacity)  # Округлення до цілого числа (бо заїхати на заправку доведеться цілу кількість разів)
+print(refill_count)
