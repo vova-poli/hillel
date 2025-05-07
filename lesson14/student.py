33aa0e4535af76aa4b60792e59d4fe8826e53375
Volodymyr Polishchuk <rot010011@gmail.com>
2025-05-07 23:00:20 +0300

    added changes

--- lesson14/student.py
+++ lesson14/student.py
@@ -13,14 +13,3 @@ class Student:
         print(f"Прізвище: {self.surname}")
         print(f"Вік: {self.age}")
         print(f"Середній бал: {self.average_grade}")
-
-
-student1 = Student("Олена", "Ковальчук", 20, 89.5)
-
-print("Початкова інформація про студента:")
-student1.display_info()
-
-student1.update_average_grade(93.2)
-
-print("\nОновлена інформація про студента:")
-student1.display_info()
